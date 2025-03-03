08648db3e8f49e34e16a7a557444056a05781f45
i1last <ac7you@gmail.com>
2025-03-03 19:53:06 +0300

    update .vscode

--- include/thread_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef THREAD_FUNCTIONS_H
-#define THREAD_FUNCTIONS_H
-
-void* keys_listener(void* arg);
-
-void* term_size_listener(void* arg);
-
-#endif
--- src/global.c
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-#include "global.h"
-
-bool IS_RUNNING = true;
-int STATE = 0;
-int VERTICAL_SELECTED_OPTION = 0;
-int HORIZONTAL_SELECTED_OPTION = 0;
